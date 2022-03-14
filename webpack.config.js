c45b826b45b4ba3d7c0766328330c67ecb759146
AlexandrLo <alexandr.lo@outlook.com>
2022-03-14 21:09:01 +0300

    Initial code commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: path.join(__dirname, "src", "js", "app.js"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          "resolve-url-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+      chunks: ["app"],
+    }),
+    new HtmlWebpackPlugin({
+      filename: "examples.html",
+      template: "src/examples.html",
+      chunks: ["app"],
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 8000,
+  },
+};
